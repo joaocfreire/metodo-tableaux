134f6fbb2613acf502b976051bcaeb138c705391
João Freire <joao.campos105115@gmail.com>
2025-09-29 22:19:12 -0300

    Ajustes no parsing

--- main.py
+++ main.py
@@ -23,56 +23,71 @@ def ramo_fechado(ramo):
 
 
 def aplica_regras(formula):
-    # Regra 4: ~~A
-    if formula.startswith('~~'):
-        return [[formula[2:]]]
+    # Tira parênteses externos
+    if formula.startswith('(') and formula.endswith(')'):
+        formula = formula.replace('(', '').replace(')', '')
 
-    # Regra 1: A ^ B
-    if '^' in formula:
-        A, B = formula.split('^', 1)
-        return [[A.strip(), B.strip()]]
-
-    elif '~(' in formula and formula.endswith(')'):
+    if formula.startswith('~(') and formula.endswith(')'):
         sub_formula = formula[2:-1].strip()
 
         # Regra 6: ~(A v B)
         if 'v' in sub_formula:
+            print("Regra 6 aplicada")
             A, B = sub_formula.split('v', 1)
             return [['~' + A.strip(), '~' + B.strip()]]
 
         # Regra 7: ~(A > B)
         elif '>' in sub_formula:
+            print("Regra 7 aplicada")
             A, B = sub_formula.split('>', 1)
             return [[A.strip(), '~' + B.strip()]]
 
         # Regra 5: ~(A ^ B)
         elif '^' in sub_formula:
+            print("Regra 5 aplicada")
             A, B = sub_formula.split('^', 1)
             return [['~' + A.strip()], ['~' + B.strip()]]
 
-    # Regra 2: A v B
-    elif 'v' in formula:
-        A, B = formula.split('v', 1)
-        return [[A.strip()], [B.strip()]]
-
     # Regra 3: A > B
     elif '>' in formula:
+        print("Regra 3 aplicada")
         A, B = formula.split('>', 1)
         return [['~' + A.strip()], [B.strip()]]
 
-    # Se a fórmula for uma negação de atômica (~P)
+    # Regra 1: A ^ B
+    elif '^' in formula:
+        print("Regra 1 aplicada")
+        A, B = formula.split('^', 1)
+        return [[A.strip(), B.strip()]]
+
+    # Regra 2: A v B
+    elif 'v' in formula:
+        print("Regra 2 aplicada")
+        A, B = formula.split('v', 1)
+        return [[A.strip()], [B.strip()]]
+
+    # Regra 4: ~~A
+    elif formula.startswith('~~'):
+        print("Regra 4 aplicada")
+        return [[formula[2:]]]
+
+    # Caso contrário
     return []
 
 
 # Algoritmo Principal
-def metodo_tableau(bd_formulas, query):
+def tableau_semantico(bd_formulas, query):
     # Inicialização do conjunto inicial de fórmulas: fórmulas do BD + negação da pergunta
     formulas = bd_formulas.copy()
-    formulas.append(('~(' + query + ')'))
+    if is_literal(query):
+        formulas.append('~' + query)
+    else:
+        formulas.append(('~(' + query + ')'))
 
     # Lista de ramos a serem explorados
     ramos_abertos = [formulas]
 
+    qtd_ramos_explorados = 0
     print(f"Iniciando Tableau para: {ramos_abertos[0]}")
 
     while ramos_abertos:
@@ -80,6 +95,7 @@ def metodo_tableau(bd_formulas, query):
         ramo_atual = ramos_abertos.pop(0)
 
         print(f"\nExplorando Ramo: {ramo_atual}")
+        qtd_ramos_explorados += 1
 
         # Verificar Fechamento: se o ramo atual contém uma contradição
         if ramo_fechado(ramo_atual):
@@ -96,6 +112,7 @@ def metodo_tableau(bd_formulas, query):
         # Conclusão do Ramo (se não houver mais decomposições possíveis)
         if formulas_para_decompor is None:
             print("RAMO ABERTO (Modelo Encontrado para a Negação)")
+            print(f"\nQuantidade de ramos explorados: {qtd_ramos_explorados}")
             return False
 
         # Aplica Regras de Decomposição
@@ -112,10 +129,14 @@ def metodo_tableau(bd_formulas, query):
 
     # Se o loop terminar e não houver retorno falso significa que todos os ramos foram fechados
     print("\nTODOS OS RAMOS FECHARAM.")
+    print(f"Quantidade de ramos explorados: {qtd_ramos_explorados}")
     return True
 
 
-print("--------------- MÉTODO DE TABLEAU ---------------")
+print("*-------------------- TABLEAU SEMÂNTICO --------------------*")
+
+print("OBS1: Digite os literais com letras maiúsculas")
+print("OBS2: Operadores aceitos: ~ / ^ / v / > / ( )")
 
 # Entrada de dados
 continua = True
@@ -128,7 +149,31 @@ while continua:
 
 query = input("\nEntre com uma pergunta: ")
 
-# Resultado do metodo de tableau
+
+#  -------------------- Exemplos vistos em aula --------------------  #
+
+# Exemplo 1 - Resultado esperado: True
+# database = ['(A v B) > C', 'C > (D^E)', 'A']
+# query = 'E v F'
+
+# Exemplo 2 - Resultado esperado: True
+# database = ['~(~A ^ ~B)', '~C > ~A']
+# query = '~(B ^ ~C) > C'
+
+# Exemplo 3 - Resultado esperado: False
+# database = ['A v C', '~B > A']
+# query = 'A > C'
+
+# Exemplo 4 - Resultado esperado: True
+# database = ['A v B v C', 'A > (C v ~D)', 'A > ~C', 'B > C', 'C > ~D']
+# query = '~D v E'
+
+# Exemplo 5 - Resultado esperado: True
+# database = ['A > (B v C)', 'B > D', 'F > (D ^ E)', 'A v F', '(C ^ A) > D']
+# query = 'D'
+
+
+# Resultado do tableau semântico
 print("\nVamos verificar se é Consequência Lógica:")
-resultado = metodo_tableau(database, query)
+resultado = tableau_semantico(database, query)
 print(f"\nResultado Final: {resultado}")
